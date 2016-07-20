a0e17e58452df677516bc88025ce2d8828af9f12
CoffeeTimeCode <coffeetimecode@gmail.com>
2016-07-20 15:47:23 -0300

    PreLoader

---
+++ Assets/Script/PreLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PreLoader : MonoBehaviour {
+
+	public Image _icones;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (IniciarTempo(3.0f));
+	}
+
+	public Vector3 Rotacionar(Vector3 _rotacao, float _speed){
+		return _rotacao * Time.deltaTime * _speed;
+	}
+
+	public IEnumerator IniciarTempo(float _tempo){
+		_icones.transform.Rotate (Rotacionar(-Vector3.forward,100.5f));
+		yield return new WaitForSeconds(_tempo);
+		Application.LoadLevel ("Scene_02");
+	}
+}
